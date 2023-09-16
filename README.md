f0620552a357828a1a50cfa64518a5e3da1d0aee
Skyler <skyler.sy.k@gmail.com>
2023-09-16 16:52:52 +0900

    Add ep01

---
+++ README.md
@@ -0,0 +1,2 @@
+# React And Next
+- 강의 [udemy-react-and-next-2023](https://www.udemy.com/course/react-next-master/?utm_source=sendgrid.com&utm_medium=email&utm_campaign=email)을 따라 학습중입니다.
---
+++ ep01/src/index.js
@@ -0,0 +1,8 @@
+import mul, { add, sub } from './math.js';
+import randomColor from 'randomcolor';
+
+console.log('안녕 npm');
+console.log(add(1, 2)); // 3
+console.log(sub(1, 2)); // -1
+console.log(mul(20, 30)); // 600
+console.log(randomColor()); // 헥스
