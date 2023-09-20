d91110aef2d7ddf42e933e08dd4bfa0d520f8c3e
Skyler <skyler.sy.k@gmail.com>
2023-09-20 19:53:56 +0900

    Add todoapp_Create

--- ep07_todoapp/src/components/TodoEditor.jsx
+++ ep07_todoapp/src/components/TodoEditor.jsx
@@ -1,10 +1,41 @@
 import './TodoEditor.css';
+import { useState, useRef } from 'react';
 
-export default function TodoEditor() {
+export default function TodoEditor({ onCreate }) {
+  const [content, setContent] = useState('');
+  const inputRef = useRef(null);
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onClickButton = (e) => {
+    // 빈 입력 방지
+    if (content === '') {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent(''); // 입력폼 초기화
+  };
+
+  const onKeyDown = (e) => {
+    // Enter키 쳤을 때도 추가
+    if (e.keyCode === 13) {
+      onClickButton();
+    }
+  };
+
   return (
     <div className='TodoEditor'>
-      <input placeholder='새로운 TODO...' />
-      <button>추가</button>
+      <input
+        placeholder='새로운 TODO...'
+        value={content}
+        onChange={onChangeContent}
+        ref={inputRef}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onClickButton}>추가</button>
     </div>
   );
 }
