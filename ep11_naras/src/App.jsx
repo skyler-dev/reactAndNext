f44ae0f39677512cf2ff9c68beb984a9fd53bf2a
Skyler <skyler.sy.k@gmail.com>
2023-09-25 13:33:05 +0900

    Add naras_SetPageRouting

--- ep11_naras/src/App.jsx
+++ ep11_naras/src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Country from './pages/Country';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return;
+  // 경로와 리액트 컴포넌트 맵핑
+  // Routes 컴포넌트 : 현재 브라우저의 url(경로)와 일치하는 Route 컴포넌트를 찾아서, element로 설정되어 있는 컴포넌트 렌더링 함
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/country' element={<Country />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- ep11_naras/src/main.jsx
+++ ep11_naras/src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+// 브라우저의 현재 주소를 저장하고 감지하는 컴포넌트
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-)
+  </BrowserRouter>
+);
