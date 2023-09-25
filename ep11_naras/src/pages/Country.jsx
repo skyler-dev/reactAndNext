9af83b07d32461637fed52868dafcba3ebe97a06
Skyler <skyler.sy.k@gmail.com>
2023-09-25 16:00:59 +0900

    Add naras_AddDynamicRouting

--- ep11_naras/src/pages/Country.jsx
+++ ep11_naras/src/pages/Country.jsx
@@ -1,3 +1,8 @@
+import { useParams } from 'react-router-dom';
+
 export default function Country() {
-  return <div>Country</div>;
+  const pararms = useParams();
+  console.log(pararms);
+
+  return <div>Country : {pararms.code}</div>;
 }
--- ep11_naras/src/pages/Search.jsx
+++ ep11_naras/src/pages/Search.jsx
@@ -1,3 +1,6 @@
+import { useSearchParams } from 'react-router-dom';
 export default function Search() {
-  return <div>Search</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  return <div>Search {searchParams.get('q')}</div>;
 }
