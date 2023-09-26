6a2886d1a94b1a60d22b388a5537f990ad0f8bca
Skyler <skyler.sy.k@gmail.com>
2023-09-26 18:42:54 +0900

    Add next_AddNavigating

--- ep12_next/pages/index.js
+++ ep12_next/pages/index.js
@@ -1,3 +1,34 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const code = 'KOR';
+
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push('/search');
+  };
+
+  return (
+    <div>
+      Home Page
+      <div>
+        <button onClick={onClickButton}>Search 페이지로 이동</button>
+      </div>
+      <div>
+        <Link href={'/search'}>Search Page 이동</Link>
+      </div>
+      <div>
+        <Link
+          href={{
+            pathname: '/country/[code]',
+            query: { code: code },
+          }}
+        >
+          {code} Page 이동
+        </Link>
+      </div>
+    </div>
+  );
 }
