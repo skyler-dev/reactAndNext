04705772c3d5076fb32be9d4374ded135dda9ba5
Skyler <skyler.sy.k@gmail.com>
2023-09-26 21:02:24 +0900

    Add next_TrySimpleSSR

--- ep12_next/pages/index.js
+++ ep12_next/pages/index.js
@@ -1,34 +1,26 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-export default function Home() {
-  const code = 'KOR';
+export default function Home({ name }) {
+  // getServerSideProps 호출 후, 이 함수는 서버에서 한번은 호출 된다 -> web api 주의
 
-  const router = useRouter();
+  console.log('HOME'); // 서버,클라이언트 둘다
 
-  const onClickButton = () => {
-    router.push('/search');
-  };
+  useEffect(() => {
+    console.log('HOME MOUNT');
+  }, []); // 클라이언트에만 (브라우저에서 하이드레이션이 끝난 이후 마운트되어서 실행)
 
-  return (
-    <div>
-      Home Page
-      <div>
-        <button onClick={onClickButton}>Search 페이지로 이동</button>
-      </div>
-      <div>
-        <Link href={'/search'}>Search Page 이동</Link>
-      </div>
-      <div>
-        <Link
-          href={{
-            pathname: '/country/[code]',
-            query: { code: code },
-          }}
-        >
-          {code} Page 이동
-        </Link>
-      </div>
-    </div>
-  );
+  return <div>Home Page{name}</div>;
 }
+// 이제 이 파일이 담당하는 페이지는 SSR 방식으로 동작하도록 설정되었다
+export const getServerSideProps = async () => {
+  // SSR을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
+  // 오직 서버에서만 실행된다 -> web api 주의
+
+  console.log('getServerSideProps Called');
+
+  return {
+    props: {
+      name: 'KOREA',
+    },
+  };
+};
