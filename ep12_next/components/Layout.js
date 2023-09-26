188cf037d4d840c02d3e30c994bde4ede211da2b
Skyler <skyler.sy.k@gmail.com>
2023-09-26 19:04:22 +0900

    Add next_AddLayout

---
+++ ep12_next/components/Layout.js
@@ -0,0 +1,10 @@
+import style from './Layout.module.css';
+
+export default function Layout({ children }) {
+  return (
+    <div className='Layout'>
+      <header className={style.header}>NARAS 🌎</header>
+      <main className={style.main}>{children}</main>
+    </div>
+  );
+}
