7bdca8179a2ce85e93e30cd2a1598e7d7076997d
Skyler <skyler.sy.k@gmail.com>
2023-09-27 19:05:46 +0900

    Add next_ApplySSGToQueryStringPage

--- ep12_next/pages/search/index.js
+++ ep12_next/pages/search/index.js
@@ -1,7 +1,27 @@
 import SubLayout from '@/components/SubLayout';
 import { fetchSearchResults } from '@/api';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-export default function Search({ countries }) {
+// 기본적으로 SSG로 동작
+export default function Search() {
+  const router = useRouter();
+  const { q } = router.query;
+
+  const [countries, setCountries] = useState([]);
+
+  const setData = async () => {
+    // 클라이언트 측에서 API호출해서 불러오기(궅이 서버측에서 렌더링할 필요까지는 없는 데이터)
+    const data = await fetchSearchResults(q);
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    if (q) {
+      setData();
+    }
+  }, [q]);
+  // 해당 페이지에 빈 div만 렌더링 되었다가(SSG 방식), API가 완료되는 시점에 나중에 클라이언트 측에서 (CSR 방식으로) 실제 데이터가 채워져 추가로 렌더링 된다
   return (
     <div>
       {countries.map((country) => (
@@ -12,21 +32,3 @@ export default function Search({ countries }) {
 }
 
 Search.Layout = SubLayout;
-
-export const getServerSideProps = async (context) => {
-  // 1. 검색 결과 불러오기 (API 호출)
-  // 2. Props로 리턴
-
-  const { q } = context.query;
-
-  let countries = [];
-  if (q) {
-    countries = await fetchSearchResults(q);
-  }
-
-  return {
-    props: {
-      countries,
-    },
-  };
-};
