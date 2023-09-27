48a31044550af58575a5ed41a401e67bb4535c68
Skyler <skyler.sy.k@gmail.com>
2023-09-27 18:24:02 +0900

    Add next_ApplyFallbackOption

--- ep12_next/pages/country/[code].js
+++ ep12_next/pages/country/[code].js
@@ -7,6 +7,21 @@ export default function Country({ country }) {
   const router = useRouter();
   const { code } = router.query;
 
+  // 데이터(props)가 없는 상태의 페이지 -> fallback 상태
+  if (router.isFallback) {
+    return <div>Loading ...</div>;
+  }
+
+  /**
+   * 아래와 같은 상황 방지
+   *
+   * 브라우저 콘솔 : GET http://localhost:3000/country/12345 500 (Internal Server Error)
+   * 터미널 : TypeError: Cannot read properties of null (reading 'commonName')
+   */
+  if (!country) {
+    return <div>존재하지 않는 국가입니다</div>;
+  }
+
   return (
     <div>
       {country.commonName} {country.officialName}
@@ -20,7 +35,7 @@ Country.Layout = SubLayout;
 export const getStaticPaths = async () => {
   return {
     paths: [{ params: { code: 'ABW' } }, { params: { code: 'KOR' } }],
-    fallback: false,
+    fallback: true,
   };
 };
 
