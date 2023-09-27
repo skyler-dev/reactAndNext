ac73f48ebb7a691bf6809a3f010a77ca75ac3ef7
Skyler <skyler.sy.k@gmail.com>
2023-09-27 14:48:46 +0900

    Add next_TrySimpleSSG

--- ep12_next/pages/about.js
+++ ep12_next/pages/about.js
@@ -1,3 +1,5 @@
+// 데이터를 불러오지 않는 SSG방식으로 동작
+
 export default function AboutPage() {
   return <div>About Page</div>;
 }
--- ep12_next/pages/index.js
+++ ep12_next/pages/index.js
@@ -10,10 +10,13 @@ export default function Home({ countries }) {
     </div>
   );
 }
-// 이제 이 파일이 담당하는 페이지는 SSR 방식으로 동작하도록 설정되었다
-export const getServerSideProps = async () => {
+
+// 이제 이 파일이 담당하는 페이지는 SSG 방식으로 동작하도록 설정되었다
+export const getStaticProps = async () => {
   // API 호출 코드가 필요함
   const countries = await fetchCountries();
+  // Next 개발 모드에서는 코드수정 결과가 바로 반영되도록 하기 위해 SSG 던 SSR 이던 모두 접속 요청을 보내면, 처음부터 다시 페이지를 생성하도록 동작한다
+  console.log('countries 데이터 불러옴');
 
   return {
     props: {
